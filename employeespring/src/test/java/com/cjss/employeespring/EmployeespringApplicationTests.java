241a7ae912456f55be35cb4512440b0de9a092dd
Satya Nagendra Kumar Surisetty <92517730+ssnk97@users.noreply.github.com>
2021-10-20 00:09:24 +0530

    Updated Patch in employee spring Boot application

---
+++ employeespring/src/test/java/com/cjss/employeespring/EmployeespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjss.employeespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
