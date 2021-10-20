e43b4b36e74805f1b18fd3e3fd9423b499a5919d
Satya Nagendra Kumar Surisetty <92517730+ssnk97@users.noreply.github.com>
2021-10-20 19:37:41 +0530

    re-uploaded

---
+++ Java Training/employeespring/src/main/java/com/cjss/employeespring/EmployeespringApplication.java
@@ -0,0 +1,13 @@
+package com.cjss.employeespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeespringApplication.class, args);
+	}
+
+}
