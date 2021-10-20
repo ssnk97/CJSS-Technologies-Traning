8ff6e15e9a063d1fdad89d65af517ec8e47186ee
Satya Nagendra Kumar Surisetty <92517730+ssnk97@users.noreply.github.com>
2021-10-20 09:28:09 +0530

    deleted employee spring
    deleted outside the directory

--- employeespring/src/main/java/com/cjss/employeespring/service/HelloService.java
+++
@@ -1,8 +0,0 @@
-package com.cjss.employeespring.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class HelloService {
-    public String getDetails() { return ("CJSS TECHNOLOGIES");}
-}
